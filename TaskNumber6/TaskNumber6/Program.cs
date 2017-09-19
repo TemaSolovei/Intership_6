81ac89da6a10e525af1e484d08aac4f2a9b29f70
TemaSolovei <temasolovei@gmail.com>
2017-09-20 01:28:55 +0500

    Дописано решение. That's all!

--- TaskNumber6/TaskNumber6/Program.cs
+++ TaskNumber6/TaskNumber6/Program.cs
@@ -10,10 +10,7 @@ namespace TaskNumber6
     {
         static void Main(string[] args)
         {
-            int a1, a2, a3, num; // a1 - первый элемент последовательности, a2 - второй, a3 - третий, num - количество элементов последовательности
-
-            IO.Input(out a1, out a2, out a3, out num);
-
+            Calculate.Solve();
         }
     }
 
@@ -21,23 +18,27 @@ namespace TaskNumber6
     {
         public static void Input(out int a1, out int a2, out int a3, out int num)
         {
-            GreenStripe(1);
+            CyanStripe(1, Console.WindowWidth);
             Console.WriteLine("Ввод начальных значений \n Не забывайте, что числа должны быть целыми");
-            GreenStripe(1);
+            CyanStripe(1, Console.WindowWidth);
 
             ParseInt("Введите первый элемент последовательности: ", out a1);
             ParseInt("Введите второй элемент последовательности: ", out a2);
             ParseInt("Введите третий элемент последовательности: ", out a3);
             ParseInt("Введите количество элементов последовательности: ", out num);
-            GreenStripe(1);
+            CyanStripe(1, Console.WindowWidth);
         }
 
-        static void GreenStripe(int height) // Рисует в консоли зелёную строку шириной height
+        static void CyanStripe(int height, int length) // Рисует в консоли Cyan строку шириной height и длиной length-1 (без переноса на следующую строку
         {
-            Console.BackgroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Cyan;
 
             for (int i = 0; i < height; i++)
             {
+                for (int j = 0; j < length-1; j++)
+                {
+                    Console.Write(" ");
+                }
                 Console.WriteLine();
             }
 
@@ -53,13 +54,69 @@ namespace TaskNumber6
             {
                 intIsOk = Int32.TryParse(Console.ReadLine(), out outInt);
                 if (!intIsOk) Console.WriteLine("Введённое значение не является числом, попробуйте ещё раз");
-            } while (intIsOk);
+            } while (!intIsOk);
         }
 
-
-        static void Output()
+        public static void Output(int[] mas) // Вывод последовательности
         {
+            CyanStripe(1, Console.WindowWidth);
+            Console.WriteLine("Вывод последовательности из " + mas.Length + " элементов");
+            CyanStripe(1, Console.WindowWidth);
 
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            if (Calculate.Check(mas)) Console.WriteLine("Подпоследовательность из чётных элементов вышеуказаной последовательности является возрастающей");
+            else Console.WriteLine("Подпоследовательность из чётных элементов вышеуказаной последовательности не является возрастающей");
+        }
+    }
+
+    class Calculate
+    {
+        static int[] CreateMas(int a1, int a2, int a3, int num)
+        {
+            int[] mas = new int[num]; // Инициализация массива
+
+            mas[0] = a1;
+            mas[1] = a2;
+            mas[2] = a3;
+
+            for (int i = 3; i < num; i++)
+            {
+                mas[i] = 13 * mas[i - 1] - 10 * mas[i - 2] + mas[i - 3];
+            }
+
+            return mas;
+        }
+
+        public static bool Check(int[] mas)
+        {
+            bool result = false; // Результат проверки на возрастание чётных элементов последовательности
+
+            for (int i = 1; i < mas.Length/2; i++)
+            {
+                if (mas[i * 2] > mas[(i * 2) - 2]) result = true;
+                else
+                {
+                    result = false;
+                    break;;
+                }
+            }
+
+            return result;
+        }
+
+        public static void Solve()
+        {
+            int a1, a2, a3, num; // a1 - первый элемент последовательности, a2 - второй, a3 - третий, num - количество элементов последовательности
+
+            IO.Input(out a1, out a2, out a3, out num); // Ввод исходных данных
+            int[] elementMas = CreateMas(a1, a2, a3, num); // Создание массива с num элементов, исходя из условий
+            IO.Output(elementMas); // Вывод последовательности и результата о том, возрастает ли последовательность из чётных элементов
         }
     }
 }
